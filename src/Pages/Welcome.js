a6800b7ab8631835ea8227a6a06ae9f6ad111b57
FredericChow00 <ruihui00.chow@gmail.com>
2022-09-05 20:38:50 +0800

    Update Welcome.js

--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -2,11 +2,15 @@ import { Link } from "react-router-dom";
 
 function Welcome() {
   return (
-    <div>
-      <div>Welcome to bridge.</div>
-      <div>
+    <div class ="h-screen bg-blue-600">
+      <div class ="font-bold text-9xl basis-10">
+        Welcome to Bridge.
+      </div>
+      <div class ="flex items-center justify-center px-6 py-6">
         <Link to="/PatientDashboard">
-          <div>Patient</div>
+          <div class = "px-8 py-8">
+            Patient
+            </div>
         </Link>
         <Link to="/TherapistDashboard">
           <button>Therapist</button>
